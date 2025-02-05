1ee1b22929884a31a2e0d159a00496d4fff0d5c3
SawNeon <maheusbastost313@gmail.com>
2025-02-05 11:31:19 -0300

    Create new query findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entites.Department;
 import model.entites.Seller;
 
 public class Program {
@@ -15,6 +18,15 @@ public class Program {
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 1: seller findByDeparment ===");
+		
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for(Seller sl : list) {
+			System.out.println(sl);
+		}
 	}
 
 }
